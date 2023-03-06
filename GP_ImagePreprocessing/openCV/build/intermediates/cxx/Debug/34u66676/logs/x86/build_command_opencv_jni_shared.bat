aad344229afe20be48f62a254417ac3427b9a55b
Huda Zain <hodazainelabden67@gmail.com>
2023-03-06 15:15:37 +0200

    Resizing a bitmap image
    Importing OpenCV Module and resizing a bitmap image.
    Could not normalize it.

---
+++ GP_ImagePreprocessing/openCV/build/intermediates/cxx/Debug/34u66676/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\AZ\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Huda\\FCIS\\4th Year\\Graduation Project\\Android Studio Repo Branch\\GP_ImagePreprocessing\\openCV\\.cxx\\Debug\\34u66676\\x86" ^
+  opencv_jni_shared
